ea47c98b6cb1fee5036d831840685f770efb3ca5
LouisPino <ljpino95@gmail.com>
2023-11-13 15:57:45 -0500

    tracking for a nd b

--- main.py
+++ main.py
@@ -17,25 +17,40 @@ if ctlr.is_kernel_driver_active(i):
 ctlr.set_configuration()
 eaddr=ep.bEndpointAddress
 aCount = 0
+bCount = 0
 aDown = False
+bDown = False
 prevAcount = -1
+prevBcount = -1
 while True:
     r=ctlr.read(eaddr, 32, 32)
     if r[5]== 47 and aDown==False:
         aCount+=1
         aDown=True
-    if r[5]==15 and aDown == True:
+    if r[5]==15:
         aDown = False
+        bDown = False
+        
     if aCount != prevAcount:  
-        print(aCount)
+        print("a ", aCount)
         prevAcount = aCount
+        
+    if r[5]== 79 and bDown==False:
+        bCount+=1
+        bDown = True
+        
+    if bCount != prevBcount:  
+        print("b ", bCount)
+        prevBcount = bCount
+    
     time.sleep(.01)
+    
     
     
 # [1]- up and down C
 # [2]- left and right C
 # 3 - LR analog
 # 4 - UD analog
-# 5 - dpad + a + b
+# 5 - dpad(0-7) + a(47) + b(79)  
 # 6 - z + L + r + start
 
