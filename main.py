bdd92c18b27b28c1fa75685f08d741b14ffddd47
LouisPino <ljpino95@gmail.com>
2023-11-13 16:17:06 -0500

    track timings for A

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import usb.core
 import usb.util
 import time
 
+
+start_time = time.time()
 ctlr = usb.core.find(idVendor=0x0e8f, idProduct=0x3013)
 if ctlr is None:
     print("No devices found.")
@@ -24,15 +26,16 @@ prevAcount = -1
 prevBcount = -1
 while True:
     r=ctlr.read(eaddr, 32, 32)
+    print(r)
     if r[5]== 47 and aDown==False:
         aCount+=1
         aDown=True
+        print(f"A #{aCount} pressed at {time.time()- start_time}")
     if r[5]==15:
         aDown = False
         bDown = False
         
     if aCount != prevAcount:  
-        print("a ", aCount)
         prevAcount = aCount
         
     if r[5]== 79 and bDown==False:
@@ -40,17 +43,22 @@ while True:
         bDown = True
         
     if bCount != prevBcount:  
-        print("b ", bCount)
         prevBcount = bCount
+        
+    if r[6] == 32:
+        break
     
     time.sleep(.01)
-    
-    
-    
-# [1]- up and down C
-# [2]- left and right C
-# 3 - LR analog
-# 4 - UD analog
-# 5 - dpad(0-7) + a(47) + b(79)  
-# 6 - z + L + r + start
 
+print(f"A: {aCount}\nB: {bCount}")
+    
+# [1]- up(32) and down(224) C
+# [2]- left(224) and right(32) C
+# 3 - LR analog (0-255)
+# 4 - UD analog (0-255)
+# 5 - dpad(0-7){+32 when a held, +64 when b held, =96 when both held} + a(47) + b(79)  {111 a and b together}
+# 6 - z(16) + L(4) + r(8) + start(32) {numbers add when held together}
+
+
+
+# decide a clean way to scale controller before more inputs
